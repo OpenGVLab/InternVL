eda055cb88471e12541e10195a723f288083573b
zhe chen <wztxy89@163.com>
2023-11-30 22:21:06 +0800

    update readme

--- segmentation/release.py
+++ segmentation/release.py
@@ -9,7 +9,24 @@ args = parser.parse_args()
 
 model = torch.load(args.filename, map_location=torch.device('cpu'))
 model = model['module']
-print(model.keys())
+
+# new_model = {}
+# for k, v in model.items():
+#     if "backbone.blocks" in k:
+#         continue
+#     if "auxiliary_head" in k:
+#         continue
+#     if "pos_embed" in k or "patch_embed" in k or "cls_token" in k:
+#         continue
+#     try:
+#         if "bn" in k:
+#             print("fp32:", k)
+#             new_model[k] = v
+#         else:
+#             new_model[k] = v
+#     except:
+#         new_model[k] = v
+# print(new_model.keys())
 
 # new_dict = {'state_dict': new_state_dict}
 torch.save(model, args.filename.replace('.pt', '_release.pt'))
