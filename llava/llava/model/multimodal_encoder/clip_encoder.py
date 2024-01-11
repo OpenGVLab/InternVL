4ca565ab134cc2d0b8193dc2866aa00205c427d9
zhe chen <wztxy89@163.com>
2024-01-11 22:06:33 +0800

    Fix vision_tower_name mismatch bug in llava

--- llava/llava/model/multimodal_encoder/clip_encoder.py
+++ llava/llava/model/multimodal_encoder/clip_encoder.py
@@ -12,6 +12,16 @@ from .internvl_14b.configuration_internvl import InternVLConfig
 from .internvl_14b.modeling_internvl import InternVLModel
 
 
+def is_intern_vit_6b_model(vision_tower_name):
+    model_names = ["intern_vit_6b", "internvit_6b", "InternViT-6B"]
+    return any(name in vision_tower_name for name in model_names)
+
+
+def is_internvl_14b_model(vision_tower_name):
+    model_names = ["internvl_14b", "intern_vl_14b", "InternVL-14B"]
+    return any(name in vision_tower_name for name in model_names)
+
+
 class CLIPVisionTower(nn.Module):
     def __init__(self, vision_tower, args, delay_load=False):
         super().__init__()
@@ -25,30 +35,26 @@ class CLIPVisionTower(nn.Module):
         if not delay_load:
             self.load_model()
         else:
-            if "EVA" in self.vision_tower_name or "eva" in self.vision_tower_name :
+            if "EVA" in self.vision_tower_name or "eva" in self.vision_tower_name:
                 self.cfg_only = EvaCLIPVisionConfig.from_pretrained(self.vision_tower_name)
-            elif "intern_vit_6b" in self.vision_tower_name:
+            elif is_intern_vit_6b_model(self.vision_tower_name):
                 self.cfg_only = InternVisionConfig.from_pretrained(self.vision_tower_name)
-            elif "internvl_14b" in self.vision_tower_name:
+            elif is_internvl_14b_model(self.vision_tower_name):
                 self.cfg_only = InternVLConfig.from_pretrained(self.vision_tower_name)
             else:
                 self.cfg_only = CLIPVisionConfig.from_pretrained(self.vision_tower_name)
 
     def load_model(self):
         if "EVA" in self.vision_tower_name or "eva" in self.vision_tower_name:
-            self.image_processor = CLIPImageProcessor.from_pretrained("./pretrained/clip-vit-large-patch14")
+            self.image_processor = CLIPImageProcessor.from_pretrained(self.vision_tower_name)
             self.vision_tower = EvaCLIPVisionModel.from_pretrained(self.vision_tower_name)
-        elif "intern_vit_6b" in self.vision_tower_name:
+        elif is_intern_vit_6b_model(self.vision_tower_name):
             self.image_processor = CLIPImageProcessor(
                 crop_size=336, do_center_crop=True, do_normalize=True, do_resize=True,
                 image_mean=[0.485, 0.456, 0.406], image_std=[0.229, 0.224, 0.225], size=336
             )
             self.vision_tower = InternVisionModel.from_pretrained(self.vision_tower_name)
-            # This operation is not compatible with DeepSpeed Zero3
-            # self.vision_tower.resize_pos_embeddings(224, 336, 14)
-            # self.vision_tower.config.image_size = 336
-            # self.vision_tower.config.force_image_size = 336
-        elif "internvl_14b" in self.vision_tower_name:
+        elif is_internvl_14b_model(self.vision_tower_name):
             self.image_processor = CLIPImageProcessor(
                 crop_size=336, do_center_crop=True, do_normalize=True, do_resize=True,
                 image_mean=[0.485, 0.456, 0.406], image_std=[0.229, 0.224, 0.225], size=336
@@ -77,7 +83,7 @@ class CLIPVisionTower(nn.Module):
         if type(images) is list:
             image_features = []
             for image in images:
-                if "internvl_14b" in self.vision_tower_name:
+                if is_internvl_14b_model(self.vision_tower_name):
                     image_forward_out, query_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0), output_hidden_states=True)
                     image_feature = self.feature_select(image_forward_out).to(image.dtype)
                     image_features.append([image_feature, query_out])
@@ -86,7 +92,7 @@ class CLIPVisionTower(nn.Module):
                     image_feature = self.feature_select(image_forward_out).to(image.dtype)
                     image_features.append(image_feature)
         else:
-            if "internvl_14b" in self.vision_tower_name:
+            if is_internvl_14b_model(self.vision_tower_name):
                 image_forward_outs, query_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
                 image_features = self.feature_select(image_forward_outs).to(images.dtype)
                 image_features = [image_features, query_outs]
@@ -121,7 +127,7 @@ class CLIPVisionTower(nn.Module):
 
     @property
     def num_patches(self):
-        if "internvl_14b" in self.vision_tower_name:
+        if is_internvl_14b_model(self.vision_tower_name):
             return (self.config.image_size // self.config.patch_size) ** 2 + 96
         else:
             return (self.config.image_size // self.config.patch_size) ** 2
