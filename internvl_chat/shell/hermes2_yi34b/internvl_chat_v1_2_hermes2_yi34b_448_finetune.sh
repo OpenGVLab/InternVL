55e2ad88c5f338261ae39079017dd3352416a2d3
zhe chen <wztxy89@163.com>
2024-02-13 22:10:49 +0800

    Update

--- internvl_chat/shell/hermes2_yi34b/internvl_chat_v1_2_hermes2_yi34b_448_finetune.sh
+++ internvl_chat/shell/hermes2_yi34b/internvl_chat_v1_2_hermes2_yi34b_448_finetune.sh
@@ -15,7 +15,7 @@ GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 export MASTER_PORT=34223
 
-OUTPUT_DIR='work_dirs/internvl_chat_hermes2_yi34b_448_finetune'
+OUTPUT_DIR='work_dirs/internvl_chat_v1_2_hermes2_yi34b_448_finetune'
 
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
