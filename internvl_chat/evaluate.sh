d6d9645941e04d041afea55d9b233a59ba25a0d9
zhe chen <wztxy89@163.com>
2024-02-21 16:23:59 +0800

    Update grounding evaluation

--- internvl_chat/evaluate.sh
+++ internvl_chat/evaluate.sh
@@ -231,6 +231,16 @@ if [ ${DATASET} == "refcoco" ]; then
     eval/refcoco/evaluate_grounding.py --checkpoint ${CHECKPOINT}
 fi
 
+if [ ${DATASET} == "refcoco-val" ]; then
+    torchrun \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --nproc_per_node=${GPUS} \
+    --master_port=${MASTER_PORT} \
+    eval/refcoco/evaluate_grounding.py --checkpoint ${CHECKPOINT} --datasets refcoco_val
+fi
+
 if [ ${DATASET} == "llava-bench" ]; then
     rm -rf results/llava_bench_results_review.jsonl
     python eval/llava_bench/evaluate_llava_bench.py --checkpoint ${CHECKPOINT}
