078e90cac07f0a93102baaa6863d87376a5918ca
zhe chen <wztxy89@163.com>
2024-04-27 17:50:50 +0800

    Clean code

--- video_retrieval/test_msrvtt.py
+++ video_retrieval/test_msrvtt.py
@@ -1,18 +1,15 @@
-import torch
-from PIL import Image
-from transformers import AutoModel, CLIPImageProcessor
-from transformers import AutoTokenizer
-
-import math
-import numpy as np
-import io
-import os
-import json
-import mmengine
-import decord
-import tqdm
 import argparse
+import io
+import json
+import math
+import os
 
+import decord
+import mmengine
+import numpy as np
+import torch
+import tqdm
+from transformers import AutoModel, AutoTokenizer, CLIPImageProcessor
 
 
 def recall_at_k(scores, positive_pairs, k):
@@ -49,39 +46,40 @@ def batchify(func, X, Y, batch_size, device, *args, **kwargs):
         results.append(result)
     return torch.cat(results)
 
-def validate_msrvtt(model, tokenizer, image_processor, root, metadata, 
-                    num_frames=1, prefix="summarize:", mode="InternVL-G", recall_k_list=[1, 5, 10],
+
+def validate_msrvtt(model, tokenizer, image_processor, root, metadata,
+                    num_frames=1, prefix='summarize:', mode='InternVL-G', recall_k_list=[1, 5, 10],
                     use_dsl=True, eval_batch_size=32):
     metadata = json.load(open(metadata))
-    
+
     video_features = []
     text_features = []
-    
+
     # compute text features
-    print("Computing text features", flush=True)
+    print('Computing text features', flush=True)
     for data in tqdm.tqdm(metadata):
-        caption = prefix + data["caption"]
+        caption = prefix + data['caption']
         input_ids = tokenizer(caption, return_tensors='pt', max_length=80,
                         truncation=True, padding='max_length').input_ids.cuda()
         with torch.no_grad():
             feat = model.encode_text(input_ids)
         text_features.append(feat.cpu())
     text_features = torch.cat(text_features)
-        
+
     # compute video features
-    print("Computing video features", flush=True)
+    print('Computing video features', flush=True)
     for data in tqdm.tqdm(metadata):
-        video_id = data["video"]
+        video_id = data['video']
         video_path = os.path.join(root, video_id)
         video_data = mmengine.get(video_path)
         video_data = io.BytesIO(video_data)
         video_reader = decord.VideoReader(video_data)
-        
+
         # uniformly sample frames
         interval = math.ceil(len(video_reader) / num_frames)
         frames_id = np.arange(0, len(video_reader), interval) + interval // 2
         assert len(frames_id) == num_frames and frames_id[-1] < len(video_reader)
-        
+
         frames = video_reader.get_batch(frames_id).asnumpy()
 
         pixel_values = image_processor(images=frames, return_tensors='pt').pixel_values
@@ -91,25 +89,25 @@ def validate_msrvtt(model, tokenizer, image_processor, root, metadata,
             feat = feat.mean(dim=0, keepdim=True)
         video_features.append(feat.cpu())
     video_features = torch.cat(video_features)
-        
-    print("Computing metrics", flush=True)
+
+    print('Computing metrics', flush=True)
     texts_emb = text_features / text_features.norm(dim=-1, keepdim=True)
     images_emb = video_features / video_features.norm(dim=-1, keepdim=True)
-    
+
     # get the score for each text and image pair
     scores = texts_emb @ images_emb.t()
 
     # construct a the positive pair matrix, which tells whether each text-image pair is a positive or not
     positive_pairs = torch.zeros_like(scores, dtype=bool)
     positive_pairs[torch.arange(len(scores)), torch.arange(len(scores))] = True
-    
+
     scores_T = scores.T
     positive_pairs_T = positive_pairs.T
-    
+
     if use_dsl:
         scores = scores * scores.softmax(dim=0)
         scores_T = scores_T * scores_T.softmax(dim=0)
-    
+
     metrics = {}
     for recall_k in recall_k_list:
         # Note that recall_at_k computes **actual** recall i.e. nb_true_positive/nb_positives, where the number
@@ -131,15 +129,14 @@ def validate_msrvtt(model, tokenizer, image_processor, root, metadata,
     print(metrics)
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='validate MSR-VTT', add_help=False)
     parser.add_argument('--video-root', type=str)
     parser.add_argument('--metadata', type=str)
-    parser.add_argument('--mode', type=str, default="InternVL-C",choices=["InternVL-C", "InternVL-G"])
+    parser.add_argument('--mode', type=str, default='InternVL-C',choices=['InternVL-C', 'InternVL-G'])
     parser.add_argument('--num-frames', type=int, default=1)
     args = parser.parse_args()
-    
+
     model = AutoModel.from_pretrained(
         'OpenGVLab/InternVL-14B-224px',
         torch_dtype=torch.bfloat16,
@@ -151,11 +148,9 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(
         'OpenGVLab/InternVL-14B-224px', use_fast=False, add_eos_token=True)
     tokenizer.pad_token_id = 0  # set pad_token_id to 0
-    
-    
 
-    metrics = validate_msrvtt(model, tokenizer, image_processor, 
-                              root=args.video_root, 
+    metrics = validate_msrvtt(model, tokenizer, image_processor,
+                              root=args.video_root,
                               metadata=args.metadata,
                               mode=args.mode,
                               num_frames=args.num_frames,)
