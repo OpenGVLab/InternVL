394a3a118227748dea65a95b16cb5b8956eb0062
zhe chen <wztxy89@163.com>
2024-06-05 02:58:57 +0800

    Fix bug

--- internvl_chat/internvl/model/internvl_chat/modeling_internvl_chat.py
+++ internvl_chat/internvl/model/internvl_chat/modeling_internvl_chat.py
@@ -137,7 +137,7 @@ class InternVLChatModel(PreTrainedModel):
         B, N, C = input_embeds.shape
         input_embeds = input_embeds.reshape(B * N, C)
 
-        if torch.dist.is_initialized() and torch.distributed.get_rank() == 0:
+        if torch.distributed.is_initialized() and torch.distributed.get_rank() == 0:
             print(f'dynamic ViT batch size: {vit_batch_size}, images per sample: {vit_batch_size / B}, dynamic token length: {N}')
 
         input_ids = input_ids.reshape(B * N)
