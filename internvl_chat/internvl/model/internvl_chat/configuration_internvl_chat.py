016ae21f18dbec7efdbf91d96de1febb6626a561
Kidand <kidand1412@sina.com>
2024-08-15 01:10:48 +0800

    Add support for the gemma2 language model in multimodal fine-tuning.

--- internvl_chat/internvl/model/internvl_chat/configuration_internvl_chat.py
+++ internvl_chat/internvl/model/internvl_chat/configuration_internvl_chat.py
@@ -8,6 +8,7 @@ import copy
 
 from internvl.model.internlm2.configuration_internlm2 import InternLM2Config
 from internvl.model.phi3.configuration_phi3 import Phi3Config
+from internvl.model.gemma2.configuration_gemma2 import Gemma2Config
 from transformers import AutoConfig, LlamaConfig, Qwen2Config
 from transformers.configuration_utils import PretrainedConfig
 from transformers.utils import logging
@@ -57,6 +58,8 @@ class InternVLChatConfig(PretrainedConfig):
             self.llm_config = Phi3Config(**llm_config)
         elif llm_config['architectures'][0] == 'Qwen2ForCausalLM':
             self.llm_config = Qwen2Config(**llm_config)
+        elif llm_config['architectures'][0] == 'Gemma2ForCausalLM':
+            self.llm_config = Gemma2Config(**llm_config)
         else:
             raise ValueError('Unsupported architecture: {}'.format(llm_config['architectures'][0]))
         self.use_backbone_lora = use_backbone_lora
