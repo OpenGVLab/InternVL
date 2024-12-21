49383dc4ae81bc8ee1c37bf16e0cd66b8618e12c
Weiyun1025 <wangweiyun@pjlab.org.cn>
2024-12-21 14:38:49 +0800

    update training shells

---
+++ internvl_chat/shell/internvl2.5_mpo/internvl2_5_78b_rpo_mmpr_plus_try11.sh
@@ -0,0 +1,83 @@
+set -x
+
+PARTITION=${PARTITION:-"Intern5"}
+GPUS=${GPUS:-512}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+QUOTA_TYPE=${QUOTA_TYPE:-"reserved"}
+NODES=$((GPUS / GPUS_PER_NODE))
+CPUS_PER_TASK=${CPUS_PER_TASK:-10}
+SRUN_ARGS=${SRUN_ARGS:-""}
+BATCH_SIZE=${BATCH_SIZE:-512}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-1}
+GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
+
+export PYTHONPATH="/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/petrel-oss-python-sdk"
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+export TRITON_CACHE_DIR="/tmp/triton_wwy/"
+export MASTER_PORT=34229
+export TF_CPP_MIN_LOG_LEVEL=3
+
+OUTPUT_DIR='work_dirs/internvl_chat_v2_5/InternVL2_5-78B-mmpr-plus-try11'
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
+
+# number of gpus: 64
+# batch size per gpu: ~4
+# gradient accumulation steps: 1
+# total batch size: ~256
+# epoch: 8
+srun -p ${PARTITION} \
+  --gres=gpu:${GPUS_PER_NODE} \
+  --nodes=${NODES} \
+  --ntasks=${GPUS} \
+  --ntasks-per-node=${GPUS_PER_NODE} \
+  --cpus-per-task=${CPUS_PER_TASK} \
+  --kill-on-bad-exit=1 \
+  --quotatype=${QUOTA_TYPE} \
+  ${SRUN_ARGS} \
+  python -u internvl/train/internvl_chat_dpo.py \
+  --model_name_or_path "/mnt/petrelfs/share_data/wangweiyun/share_internvl/InternVL2_5-78B" \
+  --conv_style "internvl2_5" \
+  --output_dir ${OUTPUT_DIR} \
+  --meta_path "/mnt/petrelfs/wangweiyun/workspace_wwy/open_source/InternVL/internvl_chat/shell/data/dev_mpo/meta_oc_data_241203_with_wh_v11.json" \
+  --overwrite_output_dir True \
+  --force_image_size 448 \
+  --down_sample_ratio 0.5 \
+  --drop_path_rate 0.4 \
+  --pad2square False \
+  --freeze_llm False \
+  --freeze_mlp False \
+  --freeze_backbone False \
+  --vision_select_layer -1 \
+  --use_data_resampling False \
+  --dataloader_num_workers 8 \
+  --bf16 True \
+  --num_train_epochs 1 \
+  --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
+  --gradient_accumulation_steps ${GRADIENT_ACC} \
+  --evaluation_strategy "no" \
+  --save_strategy "steps" \
+  --save_steps 200 \
+  --save_total_limit 100 \
+  --learning_rate 1e-6 \
+  --weight_decay 0.05 \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --max_seq_length 6544 \
+  --do_train True \
+  --grad_checkpoint True \
+  --group_by_length False \
+  --dynamic_image_size True \
+  --use_thumbnail True \
+  --ps_version 'v2' \
+  --deepspeed "zero_stage3_config_100b_1e7.json" \
+  --report_to "tensorboard" \
+  --loss_type sigmoid \
+  --sigmoid_loss_weight 1.0 \
+  --bco_pair_loss_weight 0.0 \
+  --rpo_alpha 1 \
+  --use_liger True \
+  2>&1 | tee -a "${OUTPUT_DIR}/training_log.txt"
